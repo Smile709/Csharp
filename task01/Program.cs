f3f04097fc045daa6ab9d0b46a4e7e0da9783eab
Ivan Murashov <smile709@ya.com>
2023-08-01 12:48:36 +0300

    Исправил названия Папок

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+*/
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+     Console.WriteLine("является");
+}
+else
+{
+    Console.WriteLine("не является");
+}
