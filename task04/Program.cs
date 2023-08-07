24eb3acc958995618bf10b39255ee31803f3f73a
Ivan Murashov <smile709@ya.com>
2023-08-07 12:00:51 +0300

    выполнил Task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -8,13 +8,9 @@ Console.WriteLine("Введите 3 числа");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2)
-{
-     Console.Write("max = ");
-     Console.WriteLine(number1);
-}
-else
-{
-    Console.Write("max = ");
-    Console.WriteLine(number2);
-}
+int max = number1;
+if (number1 > max) max=number1;
+if (number2 > max) max=number2;
+if (number3 > max) max=number3;
+Console.Write("max = ");
+Console.WriteLine(max);
