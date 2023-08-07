ff74e5dcd82caaa30441352cbb841b37f655ae31
Ivan Murashov <smile709@ya.com>
2023-08-07 10:36:49 +0300

    Поправил Task02

--- task02/Program.cs
+++ task02/Program.cs
@@ -4,7 +4,7 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 
 */
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите 2 числа");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
