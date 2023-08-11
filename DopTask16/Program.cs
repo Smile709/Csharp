fd1de6c7c37387805f1fee069676935af617c696
Ivan Murashov <smile709@ya.com>
2023-08-11 13:05:02 +0300

    DopTask16

---
+++ DopTask16/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Cоставить программу обмена значениями трех переменных величин а, b, c по
+следующим двум схемам: 
+    а) b присвоить значение c, а присвоить значение b, с присвоить значение а;
+    б) b присвоить значение а, с присвоить значение b, а присвоить значение с.
+    */
+System.Console.WriteLine("Введите две переменных");
+int varA = Convert.ToInt32(Console.ReadLine());
+int varB = Convert.ToInt32(Console.ReadLine());
+int varC = Convert.ToInt32(Console.ReadLine());
+int tempA = varA;
+int tempB = varB;
+varB = varC;
+varA = tempB;
+varC = tempA;
+Console.WriteLine($"Первая переменная A= {varA},{Environment.NewLine}Вторая переменная B= {varB},{Environment.NewLine}а третья переменная C= {varC}");
