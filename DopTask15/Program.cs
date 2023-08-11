65c0f8ddaaec457a8ce88d6ec7dcb45e6f8f745a
Ivan Murashov <smile709@ya.com>
2023-08-11 12:58:24 +0300

    DopTask15

---
+++ DopTask15/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Составить программу обмена значениями двух переменных величин.
+*/
+
+System.Console.WriteLine("Введите две переменных");
+int var1 = Convert.ToInt32(Console.ReadLine());
+int var2 = Convert.ToInt32(Console.ReadLine());
+int temp=var1;
+var1=var2;
+var2=temp;
+Console.WriteLine($"Первая переменная теперь {var1},{Environment.NewLine} а вторая переменная {var2}");
+
