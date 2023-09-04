68b1435e14c887f6b5ff6270367791c8d4244c51
Ivan Murashov <smile709@ya.com>
2023-09-04 19:26:08 +0300

    Add Task 42

---
+++ task42/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 42. Напишите программу, которая будет преобразовывать десятичное 
+число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+System.Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string Delenie(int a)
+{
+    string result = string.Empty;
+    int temp = 0;
+    for (int i = a; i > 0; i /= 2)
+    {
+        temp = i % 2;
+        result = temp + result;
+    }
+    return result;
+}
+
+string b = Delenie(a);
+System.Console.WriteLine(b);
