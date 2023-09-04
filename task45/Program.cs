2fa1e84989e1a6c77f85d920874dbd180f3226ec
Ivan Murashov <smile709@ya.com>
2023-09-04 21:31:59 +0300

    Add Task 45

---
+++ task45/Program.cs
@@ -0,0 +1,48 @@
+﻿/* 45. Напишите программу, которая будет создавать копию заданного 
+одномерного массива с помощью поэлементного копирования.
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введи кол-во элементов массива: ");
+int arrayL = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введи Мин значение элементов массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введи Макс значение элементов массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] GetRandomArray(int arrayL, int min, int max)
+{
+    int[] array = new int[arrayL];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+    return arrayCopy;
+}
+
+int[] array = GetRandomArray(arrayL, min, max);
+int[] userArray = CopyArray(array);
+PrintArray(array);
+PrintArray(userArray);
