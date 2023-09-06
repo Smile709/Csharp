d2f7079d876c087e6a7edfe8613e441326275332
Ivan Murashov <smile709@ya.com>
2023-09-06 16:47:26 +0300

    Add Task 49

---
+++ task49/Program.cs
@@ -0,0 +1,58 @@
+﻿/* 49. Задайте двумерный массив. Найдите элементы, 
+у которых оба индекса чётные, и замените эти элементы на их квадраты.
+*/
+
+Console.Clear();
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(0, deviation);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] DoubleEven2DArray(int [,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    int[,] result = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if(i%2==0&&j%2==0) result[i,j] = array[i,j]*array[i,j];
+            else result[i, j] = array[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] randomArray = GetRandom2DArray(5, 5, 10);
+Print2DArray(randomArray);
+int[,] newArr = DoubleEven2DArray(randomArray);
+Print2DArray(newArr);
